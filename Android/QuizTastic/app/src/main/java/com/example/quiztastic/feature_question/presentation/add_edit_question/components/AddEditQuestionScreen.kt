f5fa874dbff83b2e2dce72113985cb768e837e2c
Enaa7 <enachealexandru00@yahoo.com>
2021-10-31 13:38:27 +0200

    Added snackbar to inform the user when he did not fill all the text fields on add/edit question

--- Android/QuizTastic/app/src/main/java/com/example/quiztastic/feature_question/presentation/add_edit_question/components/AddEditQuestionScreen.kt
+++ Android/QuizTastic/app/src/main/java/com/example/quiztastic/feature_question/presentation/add_edit_question/components/AddEditQuestionScreen.kt
@@ -1,13 +1,16 @@
 package com.example.quiztastic.feature_question.presentation.add_edit_question.components
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -16,6 +19,7 @@ import com.example.quiztastic.feature_question.presentation.add_edit_question.Ad
 import com.example.quiztastic.feature_question.presentation.add_edit_question.AddEditQuestionViewModel
 import com.example.quiztastic.feature_question.presentation.util.Screen
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @Composable
 fun AddEditQuestionScreen(
@@ -29,111 +33,120 @@ fun AddEditQuestionScreen(
     val wrongAnswerThreeState = viewModel.questionWrongAnswerThree.value
     val categoryState = viewModel.questionCategory.value
 
+    val scaffoldState = rememberScaffoldState()
+
     LaunchedEffect(key1 = true) {
         viewModel.eventFlow.collectLatest { event ->
             when (event) {
                 is AddEditQuestionViewModel.UiEvent.SaveQuestion -> {
                     navController.navigateUp()
                 }
+                is AddEditQuestionViewModel.UiEvent.ShowSnackBar -> {
+                    scaffoldState.snackbarHostState.showSnackbar(event.message)
+                }
             }
         }
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+    Scaffold(
+        scaffoldState = scaffoldState
     ) {
-        Text(
-            text = "Question",
-            style = MaterialTheme.typography.h5
-        )
-        TextField(
-            value = questionState,
-            onValueChange = {
-                viewModel.onEvent(AddEditQuestionEvent.EnteredQuestion(it))
-            }
-        )
-        Text(
-            text = "Correct Answer",
-            style = MaterialTheme.typography.h5
-        )
-        TextField(
-            value = correctAnswerState,
-            onValueChange = {
-                viewModel.onEvent(AddEditQuestionEvent.EnteredCorrectAnswer(it))
-            }
-        )
-        Text(
-            text = "First wrong answer",
-            style = MaterialTheme.typography.h5
-        )
-        TextField(
-            value = wrongAnswerOneState,
-            onValueChange = {
-                viewModel.onEvent(AddEditQuestionEvent.EnteredWrongAnswerOne(it))
-            }
-        )
-        Text(
-            text = "Second wrong answer",
-            style = MaterialTheme.typography.h5
-        )
-        TextField(
-            value = wrongAnswerTwoState,
-            onValueChange = {
-                viewModel.onEvent(AddEditQuestionEvent.EnteredWrongAnswerTwo(it))
-            }
-        )
-        Text(
-            text = "Third wrong answer",
-            style = MaterialTheme.typography.h5
-        )
-        TextField(
-            value = wrongAnswerThreeState,
-            onValueChange = {
-                viewModel.onEvent(AddEditQuestionEvent.EnteredWrongAnswerThree(it))
-            }
-        )
-
-        Spacer(modifier = Modifier.height(20.dp))
-
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
+                .padding(16.dp)
         ) {
             Text(
-                text = "Category",
+                text = "Question",
                 style = MaterialTheme.typography.h5
             )
-            DropDownMenuButton(
-                selectedItem = categoryState.selectedItem,
-                isExpanded = categoryState.isExpanded,
-                onClick = {
-                    viewModel.onEvent(AddEditQuestionEvent.ClickCategoryMenu)
-                },
-                onDismiss = {
-                    viewModel.onEvent(AddEditQuestionEvent.DismissCategoryMenu)
-                },
+            TextField(
+                value = questionState,
                 onValueChange = {
-                    viewModel.onEvent(AddEditQuestionEvent.ChangeCategory(it))
+                    viewModel.onEvent(AddEditQuestionEvent.EnteredQuestion(it))
+                }
+            )
+            Text(
+                text = "Correct Answer",
+                style = MaterialTheme.typography.h5
+            )
+            TextField(
+                value = correctAnswerState,
+                onValueChange = {
+                    viewModel.onEvent(AddEditQuestionEvent.EnteredCorrectAnswer(it))
+                }
+            )
+            Text(
+                text = "First wrong answer",
+                style = MaterialTheme.typography.h5
+            )
+            TextField(
+                value = wrongAnswerOneState,
+                onValueChange = {
+                    viewModel.onEvent(AddEditQuestionEvent.EnteredWrongAnswerOne(it))
+                }
+            )
+            Text(
+                text = "Second wrong answer",
+                style = MaterialTheme.typography.h5
+            )
+            TextField(
+                value = wrongAnswerTwoState,
+                onValueChange = {
+                    viewModel.onEvent(AddEditQuestionEvent.EnteredWrongAnswerTwo(it))
+                }
+            )
+            Text(
+                text = "Third wrong answer",
+                style = MaterialTheme.typography.h5
+            )
+            TextField(
+                value = wrongAnswerThreeState,
+                onValueChange = {
+                    viewModel.onEvent(AddEditQuestionEvent.EnteredWrongAnswerThree(it))
                 }
             )
 
             Spacer(modifier = Modifier.height(20.dp))
 
-            Button(
-                onClick = {
-                    viewModel.onEvent(AddEditQuestionEvent.SaveQuestion)
-                },
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Color.Green
-                )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
             ) {
                 Text(
-                    text = "Save",
-                    style = MaterialTheme.typography.h4
+                    text = "Category",
+                    style = MaterialTheme.typography.h5
                 )
+                DropDownMenuButton(
+                    selectedItem = categoryState.selectedItem,
+                    isExpanded = categoryState.isExpanded,
+                    onClick = {
+                        viewModel.onEvent(AddEditQuestionEvent.ClickCategoryMenu)
+                    },
+                    onDismiss = {
+                        viewModel.onEvent(AddEditQuestionEvent.DismissCategoryMenu)
+                    },
+                    onValueChange = {
+                        viewModel.onEvent(AddEditQuestionEvent.ChangeCategory(it))
+                    }
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Button(
+                    onClick = {
+                        viewModel.onEvent(AddEditQuestionEvent.SaveQuestion)
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.Green
+                    )
+                ) {
+                    Text(
+                        text = "Save",
+                        style = MaterialTheme.typography.h4
+                    )
+                }
             }
         }
     }
